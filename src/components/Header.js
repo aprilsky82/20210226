b7d7cd53e943b89a78a3cb00491ddec903995e4b
HyunJong Song <songhj82@hotmail.com>
2021-02-26 19:55:33 +0900

    add styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,60 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+
+const Head = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  background-color: rgba( 20, 20, 20, 0.8);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`
+
+const List = styled.ul`
+  display: flex;
+`
+
+const Item = styled.li`
+  width: 100px;
+  height: 50px;
+  text-align: center;
+  font-size: 16px;
+  font-weight: 800;
+`
+
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+`
+
 
 const Header = () => {
     return (
-        <header>
-          <ul>
-              <li>
-                  <Link to="/">Main page</Link>
-              </li>
-              <li>
-                  <Link to="/members">Members page</Link>
-              </li>
-              <li>
-                  <Link to="/groups">Groups page</Link>
-              </li>
-              <li>
-                  <Link to="/mypage">My page</Link>
-              </li>
-          </ul>
-        </header>
+        <Head>
+          <List>
+              <Item>
+                  <SLink to="/">Main</SLink>
+              </Item>
+              <Item>
+                  <SLink to="/members">Members</SLink>
+              </Item>
+              <Item>
+                  <SLink to="/groups">Groups</SLink>
+              </Item>
+              <Item>
+                  <SLink to="/mypage">My page</SLink>
+              </Item>
+          </List>
+        </Head>
 
     );
 };
