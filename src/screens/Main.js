113ba581feac767ad6f29add6f7474d44e316e3d
HyunJong Song <songhj82@hotmail.com>
2021-02-26 20:16:50 +0900

    add getdata(news)

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React , {useState} from 'react';
+import axios from 'axios'
+
 
 const Main = () => {
+
+    const [news, setNews] = useState([])
+    const getdata = async () => {
+       await axios
+            .get("http://www.findyourapi.com/api/posts/")
+            // .then(res => console.log(">>>>>>", res))
+            .then(res => setNews(res.data))
+            .catch(err => console.log(err))
+    }
 
 
     return (
         <div>
-            <h1>main page</h1>
+            <h1>{news.length}</h1>
+            <button type={"button"} onClick={getdata}>click</button>
         </div>
     );
 };
