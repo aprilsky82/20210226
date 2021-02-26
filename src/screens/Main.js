9fffa8f4cf94183f115a3c69498364fd54d85d7a
HyunJong Song <songhj82@hotmail.com>
2021-02-26 20:44:31 +0900

    add card components (bootstrap)

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,6 +1,22 @@
 import React , {useState, useEffect} from 'react';
 import axios from 'axios'
+import styled from 'styled-components';
+import {Link} from "react-router-dom";
 
+const Container = styled.div`
+    padding: 0px 10px;
+`
+
+const Grid = styled.div`
+  margin-top: 10px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 200px);
+  grid-gap: 25px;
+`
+
+const CardView = styled.div`
+    background-color: darkgray;
+`
 
 const Main = () => {
 
@@ -18,11 +34,21 @@ const Main = () => {
     }, [])
 
     return (
-        <div>
-            <h1>{news.length}</h1>
-
-            {/*<button type={"button"} onClick={getdata}>click</button>*/}
-        </div>
+        <Container>
+            <Grid>
+                {news.map(item => (
+                    <CardView className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">{item.title}</h5>
+                            <h6 className="card-subtitle mb-2 text-muted">{item.author_name}</h6>
+                            <p className="card-text">{item.overview.slice(0, 100)}</p>
+                            <Link to="#" className="card-link">Card link</Link>
+                            <Link to="#" className="card-link">Another link</Link>
+                        </div>
+                    </CardView>
+                ))}
+            </Grid>
+        </Container>
     );
 };
 
